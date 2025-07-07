2a27e09f6d864ee31346adc05c33becc9222357d
salih.cetin <salih.cetin@forte.com.tr>
2025-07-07 17:23:32 +0300

    sunucu adresi değişti

--- src/app/pages/constant/project.constants.ts
+++ src/app/pages/constant/project.constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_CONSTANTS = {
-    API_URL: 'http://10.242.108.10:8080/api/',
-    FILE_PATH: 'http://10.242.108.10:8080',
+    API_URL: 'http://46.101.105.82:8080/api/',
+    FILE_PATH: 'http://46.101.105.82:8080',
 };
 
