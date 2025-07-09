686b3c6e708e5a1408ac6769806ca78be847b821
salih.cetin <salih.cetin@forte.com.tr>
2025-07-09 10:55:09 +0300

    update url

--- src/app/pages/constant/project.constants.ts
+++ src/app/pages/constant/project.constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_CONSTANTS = {
-    API_URL: 'https://short-phones-travel.loca.lt/api/',
-    FILE_PATH: 'https://short-phones-travel.loca.lt'
+    API_URL: 'https://cold-hoops-act.loca.lt/api/',
+    FILE_PATH: 'https://cold-hoops-act.loca.lt'
 };
 
